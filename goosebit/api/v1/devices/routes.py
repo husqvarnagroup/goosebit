cffaa1fd150bbb9c2c31151a274b91fc265b1545
Upstream Data <brett@upstreamdata.ca>
2024-11-12 10:58:44 -0700

    Add device put request for creating new devices

--- goosebit/api/v1/devices/routes.py
+++ goosebit/api/v1/devices/routes.py
@@ -13,7 +13,7 @@ from goosebit.schema.software import SoftwareSchema
 from goosebit.updater.manager import delete_devices, get_update_manager
 
 from . import device
-from .requests import DevicesDeleteRequest, DevicesPatchRequest
+from .requests import DevicesDeleteRequest, DevicesPatchRequest, DevicesPutRequest
 from .responses import DevicesResponse
 
 router = APIRouter(prefix="/devices", tags=["devices"])
@@ -53,6 +53,34 @@ async def devices_delete(_: Request, config: DevicesDeleteRequest) -> StatusResp
     dependencies=[Security(validate_user_permissions, scopes=["device.write"])],
 )
 async def devices_patch(_: Request, config: DevicesPatchRequest) -> StatusResponse:
+    for uuid in config.devices:
+        updater = await get_update_manager(uuid)
+        if config.software is not None:
+            if config.software == "rollout":
+                await updater.update_update(UpdateModeEnum.ROLLOUT, None)
+            elif config.software == "latest":
+                await updater.update_update(UpdateModeEnum.LATEST, None)
+            else:
+                software = await Software.get_or_none(id=config.software)
+                await updater.update_update(UpdateModeEnum.ASSIGNED, software)
+        if config.pinned is not None:
+            await updater.update_update(UpdateModeEnum.PINNED, None)
+        if config.name is not None:
+            await updater.update_name(config.name)
+        if config.feed is not None:
+            await updater.update_feed(config.feed)
+        if config.force_update is not None:
+            await updater.update_force_update(config.force_update)
+        if config.auth_token is not None:
+            await updater.update_auth_token(config.auth_token)
+    return StatusResponse(success=True)
+
+
+@router.put(
+    "",
+    dependencies=[Security(validate_user_permissions, scopes=["device.write"])],
+)
+async def devices_put(_: Request, config: DevicesPutRequest) -> StatusResponse:
     for uuid in config.devices:
         updater = await get_update_manager(uuid)
         if config.software is not None:
